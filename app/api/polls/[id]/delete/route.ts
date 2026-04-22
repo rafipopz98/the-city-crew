72bf16f0b844fa8bf9c9c6df65a9e404da64eb9c
rafipopz98 <198168676+Muhamma-Rafi@users.noreply.github.com>
2026-04-22 18:00:10 +0530

    added images

--- app/api/polls/[id]/delete/route.ts
+++ app/api/polls/[id]/delete/route.ts
@@ -1,14 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/db/mongoose";
 import { PollModel } from "@/lib/models/Polls";
 import { getUserFromRequest } from "@/utils/getUserFromRequest";
 
 export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } },
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> },
 ) {
   try {
     await connectDB();
+
+    const { id } = await context.params; // ✅ important fix
 
     // 🔐 Auth
     const user = await getUserFromRequest();
@@ -22,7 +24,7 @@ export async function DELETE(
       return NextResponse.json({ message: "Forbidden" }, { status: 403 });
     }
 
-    const poll = await PollModel.findById(params.id);
+    const poll = await PollModel.findById(id); // use id here
 
     if (!poll) {
       return NextResponse.json({ message: "Poll not found" }, { status: 404 });
