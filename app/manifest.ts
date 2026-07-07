6134ac1fdd9bcde9cdf41922c0ae02645552d27a
rafipopz98 <itsrealrafi@gmail.com>
2026-07-07 17:44:49 +0530

    added more seo

---
+++ app/manifest.ts
@@ -0,0 +1,35 @@
+import type { MetadataRoute } from "next";
+import { SITE_CONFIG } from "@/lib/site";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: SITE_CONFIG.name,
+    short_name: SITE_CONFIG.shortName,
+    description: SITE_CONFIG.description,
+
+    start_url: "/",
+
+    display: "standalone",
+
+    background_color: "#FFF5E5",
+    theme_color: "#06182e",
+
+    icons: [
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  };
+}
